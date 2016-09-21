3471ce797adc0192dfd8be3320dd3e5bbbf96c20
Emilie Sicilia <emilie.sicilia@gmail.com>
2016-09-21 21:20:34 +0200

    front : tournament list

--- front/app/tournaments/tournamentsList.js
+++ front/app/tournaments/tournamentsList.js
@@ -3,12 +3,49 @@
 angular.module('tournamentsList', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/tournaments/list', {
-    templateUrl: 'tournaments/tournaments-list.html',
-    controller: 'TournamentsListCtrl'
-  });
+    $routeProvider.when('/tournaments/list', {
+        templateUrl: 'tournaments/tournaments-list.html',
+        controller: 'TournamentsListCtrl'
+    });
 }])
 
-.controller('TournamentsListCtrl', [function() {
+.controller('TournamentsListCtrl', ['$http', function($http) {
+    var scope = this;
+    scope.tournaments = [];
+    $http.get('/tournaments/tournamentsList.json').success(function(data){
+        scope.tournaments = data;
+    });
+}])
 
-}]);
+.directive("futureTournaments", function(){
+    return {
+        restrict: 'E',
+        templateUrl: "tournaments/future-tournaments.html"
+    };
+})
+
+.directive("pastTournaments", function(){
+    return {
+        restrict: 'E',
+        templateUrl: "tournaments/past-tournaments.html"
+
+    };
+})
+
+.filter('isFuture', function() {
+  return function(items, dateFieldName) {
+    return items.filter(function(item){
+      return moment(item[dateFieldName || 'date']).isSameOrAfter(new Date(),'day');
+    })
+  }
+})
+
+.filter('isPast', function() {
+  return function(items, dateFieldName) {
+    return items.filter(function(item){
+      return moment(item[dateFieldName || 'date']).isBefore(new Date(), 'day');
+    })
+  }
+})
+
+;
