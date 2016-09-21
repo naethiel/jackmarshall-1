8677ff124b7e65ac4a03b04bf583ad72ae9163bd
Emilie Sicilia <emilie.sicilia@gmail.com>
2016-09-21 19:34:35 +0200

    freshstart

--- tournaments/game.go
+++
@@ -1,6 +0,0 @@
-package main
-
-type Game struct {
-	Table   string   `json:"table" bson:"table"`
-	Pairing []Result `json:"pairing" bson:"pairing"`
-}
--- tournaments/player.go
+++
@@ -1,9 +0,0 @@
-package main
-
-type Player struct {
-	Name     string   `json:"name"`
-	Faction  string   `json:"faction"`
-	PayedFee bool     `json:"payed_fee"`
-	Lists    []string `json:"lists"`
-	Leave    bool     `json:"leave"`
-}
