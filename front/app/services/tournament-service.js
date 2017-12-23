daa8faeb53d4cf650ae4c554dc38a6fe256c2611
chibimi <emilie.sicilia@gmail.com>
2017-12-23 01:52:24 +0100

    update drag and drop and round check function

--- front/app/services/tournament-service.js
+++ front/app/services/tournament-service.js
@@ -94,13 +94,16 @@ app.service('TournamentService', ['$http', 'AuthService', function($http, authSe
 					break;
 				}
 			}
+			console.log("toto")
+
+			verifyVictoryPoint(tournament, index);
+
 			tournament.rounds[index].games.forEach(function(game){
 				verifyParing(tournament, game, index);
-				verifyTable(tournament, game, index);
-				verifyVictoryPoint(tournament, index);
-				verifyOrigin(game,index);
-				verifyList(tournament, game.results[0].player, index);
-				verifyList(tournament, game.results[1].player, index);
+				// verifyTable(tournament, game, index);
+				verifyOrigin(tournament,game,index);
+				// verifyList(tournament, game.results[0].player, index);
+				// verifyList(tournament, game.results[1].player, index);
 			});
 		}
 	};
@@ -112,21 +115,33 @@ app.service('TournamentService', ['$http', 'AuthService', function($http, authSe
 function verifyParing(tournament, g, index){
 	g.errorPairing = false;
 	for (var i=0; i < index; i++){
-		var round = tournament.rounds[i];
-		if (round.number===index){
-			return;
-		}
-		round.games.forEach(function(game){
-			if ((g.results[0].player.name === game.results[0].player.name && g.results[1].player.name === game.results[1].player.name) ||
-			(g.results[0].player.name === game.results[1].player.name && g.results[1].player.name === game.results[0].player.name)) {
+		tournament.rounds[i].games.forEach(function(game){
+			if ((g.results[0].player === game.results[0].player && g.results[1].player === game.results[1].player) ||
+			(g.results[0].player === game.results[1].player && g.results[1].player === game.results[0].player)) {
 				g.errorPairing = true;
 			}
 		});
 	}
 };
 
-function verifyOrigin(g, index){
-	g.errorOrigin = (g.results[0].player.origin == g.results[1].player.origin && g.results[0].player.origin != "");
+function verifyVictoryPoint(tournament, index){
+	for (var i=0; i < index; i++){
+		tournament.rounds[i].games.forEach(function(game){
+			tournament.players[game.results[0].player].victory_points = game.results[0].victory_points
+			tournament.players[game.results[1].player].victory_points = game.results[1].victory_points
+		});
+	}
+	tournament.rounds[index].games.forEach(function(game){
+		game.errorVP = false;
+		if (tournament.players[game.results[0].player].victory_points != tournament.players[game.results[1].player].victory_points){
+			game.errorVP = true;
+		}
+	});
+};
+
+
+function verifyOrigin(tournament,g, index){
+	g.errorOrigin = (tournament.players[g.results[0].player].origin == tournament.players[g.results[1].player].origin && tournament.players[g.results[0].player].origin != "");
 };
 
 function verifyList(tournament, player, index){
@@ -153,38 +168,12 @@ function verifyList(tournament, player, index){
 		});
 	}
 };
-function verifyVictoryPoint(tournament, index){
-	console.log("index ",index)
-	var vpByPlayer = new Map();
-	for (var i=0; i <= index; i++){
-		var round = tournament.rounds[i];
-		round.games.forEach(function(game){
-			var vp0 = getNum(game.results[0].victory_points) + getNum(vpByPlayer.get(game.results[0].player.id));
-			var vp1 = getNum(game.results[1].victory_points) + getNum(vpByPlayer.get(game.results[1].player.id));
-			// var vp1 = vpByPlayer.get(game.results[1].player.id)+game.results[1].player.victory_points;
-			// console.log("round ", i, " p0: ",game.results[0].player.name, " jsonvp: ", getNum(game.results[0].victory_points), " oldvp: ", getNum(vpByPlayer.get(game.results[0].player.id)), " vp: ", vp0)
-			// console.log("round ", i, " p1: ",game.results[1].player.name, " jsonvp: ", getNum(game.results[1].victory_points), " oldvp: ", getNum(vpByPlayer.get(game.results[1].player.id)), " vp: ", vp1)
-
-			vpByPlayer.set(game.results[0].player.id, vp0);
-			vpByPlayer.set(game.results[1].player.id, vp1);
-			game.results[0].player.vp = vp0;
-			game.results[1].player.vp = vp1;
-		});
-		round.games.forEach(function(game){
-			console.log(vpByPlayer.get(game.results[0].player.id), " ",vpByPlayer.get(game.results[1].player.id))
-			if (vpByPlayer.get(game.results[0].player.id) != vpByPlayer.get(game.results[1].player.id)) {
-				console.log("err vp")
-				game.errorVP = true;
-			}
-		});
-	}
-};
 
 function getNum(val) {
-   if (isNaN(val)) {
-     return 0;
-   }
-   return val;
+	if (isNaN(val)) {
+		return 0;
+	}
+	return val;
 }
 
 function verifyTable(tournament, g, index){
--- front/app/views/tournamentDetails/games/games-controller.js
+++ front/app/views/tournamentDetails/games/games-controller.js
@@ -6,6 +6,13 @@ app.controller('GamesCtrl', ["$rootScope", "TournamentService", function ($rootS
     scope.game = {};
     scope.errorUpdate = undefined;
 
+    this.Player = function(index){
+        return scope.tournament.players[scope.game.results[index].player]
+    };
+    this.Table = function(){
+        return scope.tournament.tables[scope.game.table]
+    };
+
     this.changeRes = function(game, player_index, opponent_index){
         if (game.results[player_index].victory_points===0 ||game.results[player_index].victory_points===false){
             game.results[player_index].victory_points = 1;
@@ -20,28 +27,16 @@ app.controller('GamesCtrl', ["$rootScope", "TournamentService", function ($rootS
     };
 
     this.onDropComplete=function(source, destination){
-        var sourceTemp = JSON.parse(JSON.stringify(source));
-        source.name = destination.name;
-        source.faction = destination.faction;
-        source.payed_fee = destination.payed_fee;
-        source.lists = destination.lists;
-        source.leave = destination.leave;
-        source.origin = destination.origin;
-        source.id = destination.id;
-        destination.name = sourceTemp.name;
-        destination.faction = sourceTemp.faction;
-        destination.payed_fee = sourceTemp.payed_fee;
-        destination.lists = sourceTemp.lists;
-        destination.leave = sourceTemp.leave;
-        destination.origin = sourceTemp.origin;
-        destination.id = sourceTemp.id;
+        var temp = source.player;
+        source.player = destination.player;
+        destination.player = temp;
         this.updateGame()
         tournamentService.verifyRound(scope.tournament, scope.roundNumber);
     };
 
     this.updateGame = function(){
         scope.errorUpdate = null;
-        tournamentService.update(scope.tournament).then(function(id){
+        tournamentService.update(scope.tournament).then(function(){
             $rootScope.$emit("UpdateResult");
         }).catch(function(err){
             scope.errorUpdate = true;
