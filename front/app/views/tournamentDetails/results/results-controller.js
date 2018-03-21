3a3d24c1edf2be27fc91d79fe2fb761406943988
chibimi <emilie.sicilia@gmail.com>
2018-03-21 08:41:03 +0100

    wip add best player info

--- front/app/views/tournamentDetails/results/results-controller.js
+++ front/app/views/tournamentDetails/results/results-controller.js
@@ -16,11 +16,50 @@ app.controller('ResultsCtrl', ["$rootScope", "$routeParams", "$uibModal", "uuid"
     $rootScope.$on("UpdateResult", function(){
         scope.error = null;
         tournamentService.getResults($routeParams.id).then(function(players){
-            scope.players = players;
+            scope.players = players
+            scope.addBest(scope.players)
         }).catch(function(err){
             scope.error = err;
         });
     });
+
+    scope.addBest = function(players){
+        var vp = {index: -1,max: 0}
+        var sos = {index: -1,max: 0}
+        var sp = {index: -1,max: 0}
+        var dp = {index: -1,max: 0}
+        var ck = {index: -1,max: 0}
+
+        angular.forEach(players, function(player, id) {
+            if (player.result.victory_points >= vp.max) {
+                vp.index = id
+                vp.max = player.result.victory_points
+            }
+            if (player.result.sos >= sos.max) {
+                sos.index = id
+                sos.max = player.result.sos
+            }
+            if (player.result.scenario_points >= sp.max) {
+                sp.index = id
+                sp.max = player.result.scenario_points
+            }
+            if (player.result.destruction_points >= dp.max) {
+                dp.index = id
+                dp.max = player.result.destruction_points
+            }
+            if (player.result.caster_kills >= ck.max) {
+                ck.index = id
+                ck.max = player.result.caster_kills
+            }
+        });
+
+        players[vp.index].result.best_vp = true
+        players[sos.index].result.best_sos = true
+        players[sp.index].result.best_sp = true
+        players[dp.index].result.best_dp = true
+        players[ck.index].result.best_ck = true
+
+    }
 
     scope.bbCodeResults = function(players) {
         var params = {
